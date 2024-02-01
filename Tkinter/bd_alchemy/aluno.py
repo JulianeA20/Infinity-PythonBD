9c2471d1225219b78e64c8dd9ca471779d7c8077
Juliane Almeida <87144780+JulianeA20@users.noreply.github.com>
2024-02-01 15:30:25 -0300

    Delete Tkinter directory

--- Tkinter/bd_alchemy/aluno.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import *
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-class Aluno(Base):
-    __tablename__="alunos"
-    id = Column(Integer,primary_key = True)
-    nome = Column(String)
-        
